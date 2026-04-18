7090f5190400b7e9b422db15194144f805c1366f
lowe-gon <lowe.bg13@gmail.com>
2026-04-19 06:43:05 +0800

    feat: add is the user is already loan

--- app/(app)/(user)/(tabs)/index.tsx
+++ app/(app)/(user)/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Pressable } from '@/components/ui/pressable';
-import { AntDesign, Feather, FontAwesome } from '@expo/vector-icons';
+import { cn } from '@/libs/cn';
+import { AntDesign, Entypo, Feather, FontAwesome } from '@expo/vector-icons';
 import { useAssets } from 'expo-asset';
 import { Image } from 'expo-image';
 import { Link } from 'expo-router';
@@ -8,6 +9,7 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   const [assets] = useAssets([require('@/assets/images/dog-home.png')]);
+  const isFirstTime = false;
 
   return (
     <View className="bg-background flex-1">
@@ -34,66 +36,115 @@ export default function HomeScreen() {
               ) : null}
             </View>
             <View className="flex-1 rounded-tr-3xl rounded-br-3xl rounded-bl-4xl bg-white p-4">
-              <Text className="font-quicksand-bold text-base">Hi, Jack!</Text>
-              <Text className="font-quicksand-medium text-sm">
-                I&apos;ve got something special for you!
+              <Text
+                className={cn(
+                  isFirstTime ? 'font-quicksand-bold text-base' : 'font-quicksand-medium text-xs',
+                )}>
+                {isFirstTime ? 'Hi, Juan!' : 'Welcome'}
+              </Text>
+              <Text
+                className={cn(
+                  isFirstTime ? 'font-quicksand-medium text-sm' : 'font-quicksand-bold text-base',
+                )}>
+                {isFirstTime ? 'I&apos;ve got something special for you!' : 'Juan Dela Cruz'}
               </Text>
             </View>
           </View>
 
-          {/* Loan Card */}
-          <View className="relative mb-12">
-            <View className="z-50 rounded-3xl bg-white p-4 shadow-md shadow-gray-300">
-              <View className="flex flex-col items-center justify-center gap-2">
-                <View className="flex flex-row items-center gap-1">
-                  <Text className="font-quicksand-medium text-base">Get line of credit up to</Text>
-                  <Feather name="trending-up" size={20} color="#002bae" />
+          {/* First Time Loan */}
+          {isFirstTime && (
+            <React.Fragment>
+              <View className="relative mb-12">
+                <View className="z-50 rounded-3xl bg-white p-4 shadow-md shadow-gray-300">
+                  <View className="flex flex-col items-center justify-center gap-2">
+                    <View className="flex flex-row items-center gap-1">
+                      <Text className="font-quicksand-medium text-base">
+                        Get line of credit up to
+                      </Text>
+                      <Feather name="trending-up" size={20} color="#002bae" />
+                    </View>
+                    <Text className="font-quicksand-bold text-4xl">
+                      <Text className="text-secondary">₱</Text> 15,000
+                    </Text>
+                    <Link href="/(app)/(user)/apply_loans" push asChild>
+                      <Pressable className="bg-secondary flex w-full flex-row items-center justify-center rounded-xl py-4">
+                        <Text className="font-quicksand-bold text-base">GET THE FIRST LOAN</Text>
+                      </Pressable>
+                    </Link>
+                  </View>
                 </View>
-                <Text className="font-quicksand-bold text-4xl">
-                  <Text className="text-secondary">₱</Text> 15,000
-                </Text>
-                <Link href="/(app)/(user)/apply_loans" push asChild>
-                  <Pressable className="bg-secondary flex w-full flex-row items-center justify-center rounded-xl py-4">
-                    <Text className="font-quicksand-bold text-base">GET THE FIRST LOAN</Text>
+                <View className="absolute right-2 -bottom-2 left-2 z-40 h-32 rounded-3xl bg-white p-4 shadow-md shadow-gray-300"></View>
+                <View className="absolute right-4 -bottom-4 left-4 h-32 rounded-3xl bg-white p-4 shadow-md shadow-gray-300"></View>
+              </View>
+
+              <View className="flex w-full flex-row flex-wrap items-center gap-3">
+                <View className="flex-1 gap-4 overflow-hidden rounded-xl bg-[#f2f2f2] p-4">
+                  <Text className="font-quicksand-medium z-50 text-sm">Low interest rate </Text>
+                  <Text className="font-quicksand-semibold z-50 text-base leading-4">
+                    Save more on every payment.
+                  </Text>
+                  <View className="-z-50 -mt-8 flex w-full flex-row justify-end">
+                    <FontAwesome name="calculator" size={60} color="#d1d5dc" />
+                  </View>
+                </View>
+                <View className="flex-1 gap-4 overflow-hidden rounded-xl bg-[#f2f2f2] p-4">
+                  <Text className="font-quicksand-medium text-sm">Flexible terms</Text>
+                  <Text className="font-quicksand-semibold text-base leading-4">
+                    Loans that adapt to you.
+                  </Text>
+                  <View className="-z-50 -mt-8 flex w-full flex-row justify-end">
+                    <FontAwesome name="calendar" size={60} color="#d1d5dc" />
+                  </View>
+                </View>
+                <View className="w-full rounded-xl bg-[#f2f2f2] px-4 py-6">
+                  <View className="flex flex-row items-center gap-3">
+                    <AntDesign name="clock-circle" size={60} color="#d1d5dc" />
+                    <View className="flex-1 flex-col gap-4">
+                      <Text className="font-quicksand-medium text-sm">Fast Access</Text>
+                      <Text className="font-quicksand-semibold text-base leading-4">
+                        Approved in 5–20 minutes.
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </React.Fragment>
+          )}
+          {!isFirstTime && (
+            <View className="z-50 gap-4 rounded-3xl bg-white p-5 shadow-md shadow-black/20">
+              <View className="bg-muted flex-col gap-3 rounded-3xl p-5">
+                <View className="flex flex-row items-center justify-between">
+                  <Text className="font-quicksand-semibold text-sm">Loan status</Text>
+                  <View className="bg-secondary rounded-full px-4 py-1">
+                    <Text className="font-quicksand-semibold text-sm text-white">Active</Text>
+                  </View>
+                </View>
+                <View className="flex flex-row items-center justify-between">
+                  <Text className="font-quicksand-semibold text-sm">Repayment date</Text>
+
+                  <Text className="font-quicksand-bold text-base">20-06-2026</Text>
+                </View>
+                <View className="flex flex-row items-center justify-between">
+                  <Text className="font-quicksand-semibold text-sm">Repayment amount</Text>
+
+                  <Text className="font-quicksand-bold text-base">₱ 5,000</Text>
+                </View>
+
+                <Link href={'/'} push asChild>
+                  <Pressable className="flex w-full flex-row items-center justify-center gap-1 rounded-full bg-white py-4">
+                    <Text className="text-primary font-quicksand-bold text-base">LOAN DETAILS</Text>
+                    <Entypo name="chevron-thin-right" size={16} color={'#032aa7'} />
                   </Pressable>
                 </Link>
               </View>
-            </View>
-            <View className="absolute right-2 -bottom-2 left-2 z-40 h-32 rounded-3xl bg-white p-4 shadow-md shadow-gray-300"></View>
-            <View className="absolute right-4 -bottom-4 left-4 h-32 rounded-3xl bg-white p-4 shadow-md shadow-gray-300"></View>
-          </View>
 
-          <View className="flex w-full flex-row flex-wrap items-center gap-3">
-            <View className="flex-1 gap-4 overflow-hidden rounded-xl bg-[#f2f2f2] p-4">
-              <Text className="font-quicksand-medium z-50 text-sm">Low interest rate </Text>
-              <Text className="font-quicksand-semibold z-50 text-base leading-4">
-                Save more on every payment.
-              </Text>
-              <View className="-z-50 -mt-8 flex w-full flex-row justify-end">
-                <FontAwesome name="calculator" size={60} color="#d1d5dc" />
-              </View>
+              <Link href={'/'} push asChild>
+                <Pressable className="bg-secondary flex flex-row items-center justify-center rounded-full py-4">
+                  <Text className="font-quicksand-bold text-base">MAKE PAYMENT</Text>
+                </Pressable>
+              </Link>
             </View>
-            <View className="flex-1 gap-4 overflow-hidden rounded-xl bg-[#f2f2f2] p-4">
-              <Text className="font-quicksand-medium text-sm">Flexible terms</Text>
-              <Text className="font-quicksand-semibold text-base leading-4">
-                Loans that adapt to you.
-              </Text>
-              <View className="-z-50 -mt-8 flex w-full flex-row justify-end">
-                <FontAwesome name="calendar" size={60} color="#d1d5dc" />
-              </View>
-            </View>
-            <View className="w-full rounded-xl bg-[#f2f2f2] px-4 py-6">
-              <View className="flex flex-row items-center gap-3">
-                <AntDesign name="clock-circle" size={60} color="#d1d5dc" />
-                <View className="flex-1 flex-col gap-4">
-                  <Text className="font-quicksand-medium text-sm">Fast Access</Text>
-                  <Text className="font-quicksand-semibold text-base leading-4">
-                    Approved in 5–20 minutes.
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </View>
+          )}
         </View>
       </View>
     </View>
